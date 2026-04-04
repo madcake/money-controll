db90d9a325ad81c73ef567a86ec8307e23c1e263
madcake <133312+madcake@users.noreply.github.com>
2026-04-04 08:31:40 +0700

    Add navigation icon

--- composeApp/src/commonMain/kotlin/shiny/mc/feature/categories/CategoriesNavScreen.kt
+++ composeApp/src/commonMain/kotlin/shiny/mc/feature/categories/CategoriesNavScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -44,6 +45,11 @@ fun CategoriesNavScreen(
         topBar = {
             CenterAlignedTopAppBar(
                 title = { Text("Select categories") },
+                navigationIcon = {
+                    IconButton(onClick = onCancel) {
+                        Icon(imageVector = Icons.Default.ArrowBackIosNew, contentDescription = "")
+                    }
+                },
                 actions = {
                     if (selected.isNotEmpty()) {
                         IconButton(
